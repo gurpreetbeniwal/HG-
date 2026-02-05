43e977b69603841acff4f0a04c014dce29def670
Gurpreet Beniwal <145149868+gurpreetbeniwal@users.noreply.github.com>
2026-02-05 12:45:26 +0530

    Update db.js

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb+srv://pulkit:pulkitHG70@cluster0.nbuqp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'); // Remove deprecated options
+        await mongoose.connect('mongodb+srv:'); // Remove deprecated options
         console.log('MongoDB connected successfully');
     } catch (error) {
         console.error('MongoDB connection error:', error);
